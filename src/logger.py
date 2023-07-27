1c3afb2998c6aab0a64cc07672c331e38b1d9df9
mohithmenon1998 <mohithmenon1998@gmail.com>
2023-07-27 18:38:23 +0530

    updated logger.py and exception.py

--- src/logger.py
+++ src/logger.py
@@ -7,12 +7,15 @@ LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_path = os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    filename = LOG_FILE_path ,
+
+    filename = LOG_FILE_PATH ,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO,
-    format="[%(asctime)s] %(lineo)d %(name)s - %(levelname)s - %(message)s",
+
 ) 
 
- 
+if __name__=="__main__":
+    logging.info("Logging started")
